d0817134d8a575c955617b660a9b48075dd1179a
Moisés Hiraldo <moiseshiraldo@gmail.com>
2015-06-23 22:34:25 +0100

    Update css and js

--- static/js/global.js
+++ static/js/global.js
@@ -36,4 +36,15 @@ $(document).ready(function(){
         }
     });
 
+    // Google +1 button
+    (function() {
+        var po = document.createElement('script');
+        po.type = 'text/javascript';
+        po.async = true;
+        po.src = 'https://apis.google.com/js/plusone.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(po, s);
+    })();
+
+
 });
